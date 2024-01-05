6c77da876a60f12f3f5ce21ed395b3000a1df8af
jassyio <joejassyio@gmail.com>
2024-01-05 20:03:22 +0300

    db_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,11 +70,10 @@ class FileStorage:
         self.reload()
     def get(self, cls, id):
         """ retrieves """
-        if cls in classes.values() and id and type(id) == str:
-            d_obj = self.all(cls)
-            for key, value in d_obj.items():
-                if key.split(".")[1] == id:
-                    return value
+        if cls and id:
+            
+            key= "{}.{}".format(cls.__name__, id)
+            return self.__objects.get(key)
         return None
 
     def count(self, cls=None):
